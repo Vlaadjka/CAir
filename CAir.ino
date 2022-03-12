527fd29ff87b882d941e6033264a3aeca9cfe4e6
Vlaadjka <vladislavs.maksimcuks@gmail.com>
2022-03-12 14:56:03 +0200

    Add CO2, update formatting and sensor setup

--- CAir.ino
+++ CAir.ino
@@ -7,38 +7,75 @@
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=*/ 15, /*DC=*/ 27, /*RST=26*/ 26, /*BUSY=*/ 25));
 
 #include "DHT.h"
+DHT dht(GPIO_NUM_5, DHT22);
 
-#define DHTPIN 5
-#define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
+const int CO2_RX_PIN = GPIO_NUM_16;
+const int CO2_TX_PIN = GPIO_NUM_17;
+const int CO2_POWER_PIN = GPIO_NUM_26;
+const int CO2_POWER_PIN_FREQ = 5000;
+const int CO2_POWER_PIN_CH = 0;
+const int CO2_POWER_PIN_CH_RES = 8;
 
-#define BAUDRATE 9600
+HardwareSerial co2Serial(1);
 
-void setupDisplay()
+byte co2Response[7];
+
+const int READ_ERROR = -1;
+
+void co2_write_request()
 {
-  display.init(BAUDRATE); // uses standard SPI pins, e.g. SCK(18), MISO(19), MOSI(23), SS(5)
-  SPI.end(); // release standard SPI pins
-  SPI.begin(13, 12, 14, 15); // map and init SPI pins SCK(13), MISO(12), MOSI(14), SS(15)
-  display.setRotation(1); // Display is installed upside down
-  Serial.println("Display ready");
+  byte co2ReadRequest[8] = {0xFE, 0x04, 0x00, 0x03, 0x00, 0x01, 0xD5, 0xC5};
+
+  while (co2Serial.available()) // Flush whatever we have before writing
+    co2Serial.read();
+  co2Serial.write(co2ReadRequest, 8);
 }
 
-void setupHumidityTempSensor()
+int co2_read_response()
 {
-  dht.begin();
-}
-
-void setup()
-{
-  Serial.begin(BAUDRATE);
+  unsigned long startTime = millis();
+  int waitMs = 500;
+  int waitBytes = 7;
   
-  setupDisplay();
-  drawLowBattery();
+  while (co2Serial.available() < waitBytes) //Wait full response
+  {
+    unsigned long timeout = (unsigned long)((long)millis() - (long)startTime);
+    if(waitMs < timeout) {       
+      return READ_ERROR;
+    }
+  }
 
-  setupHumidityTempSensor();
+  for (int i = 0; i < 7; i++)
+  {
+    co2Response[i] = co2Serial.read();
+  }
+
+  return waitBytes;
 }
 
-void drawLowBattery()
+int co2_get_measurement()
+{
+  co2_write_request();
+  if (co2_read_response() != READ_ERROR)
+  {
+    Serial.println();
+    for (int i = 0; i < 7; i++) {
+      Serial.print(co2Response[i], HEX);
+      Serial.print(" ");    
+    }
+    Serial.println();
+      
+    int high = co2Response[3];
+    int low = co2Response[4];
+  
+    return high * 256 + low;   
+  } else {
+    Serial.println("CO2 Read error!");
+    return READ_ERROR;
+  }
+}
+
+void draw_low_battery()
 {
   display.firstPage();
   do
@@ -49,23 +86,69 @@ void drawLowBattery()
   while (display.nextPage());
 }
 
-float getHumidity()
+float get_humidity()
 {
   float humidity = dht.readHumidity();
   Serial.println("Humidity: " + String(humidity));
   return humidity;
 }
 
-float getTemperature()
+float get_temperature()
 {
   float temperature = dht.readTemperature();
   Serial.println("Temperature: " + String(temperature));
   return temperature;
 }
 
+void co2_power_on()
+{
+  ledcWrite(CO2_POWER_PIN_CH, 255);
+  delay(50);
+  Serial.println("CO2 Power ON");
+}
+
+void co2_power_off()
+{
+  ledcWrite(CO2_POWER_PIN_CH, 0);
+  delay(50);
+  Serial.println("CO2 Power OFF");
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Display
+  display.init(9600); // uses standard SPI pins, e.g. SCK(18), MISO(19), MOSI(23), SS(5)
+  SPI.end(); // release standard SPI pins
+  SPI.begin(13, 12, 14, 15); // map and init SPI pins SCK(13), MISO(12), MOSI(14), SS(15)
+  display.setRotation(1); // Display is installed upside down
+  Serial.println("Display ready");
+  
+  draw_low_battery();
+
+  // DHT
+  dht.begin();
+
+  // CO2
+  ledcSetup(CO2_POWER_PIN_CH, CO2_POWER_PIN_FREQ, CO2_POWER_PIN_CH_RES);
+  ledcAttachPin(CO2_POWER_PIN, CO2_POWER_PIN_CH);
+  co2Serial.begin(9600, SERIAL_8N1, CO2_RX_PIN, CO2_TX_PIN);
+}
+
 void loop()
 {
-  getHumidity();
-  getTemperature();
-  delay(2000);
+  delay(50);
+  get_humidity();
+  delay(50);
+  get_temperature();
+  
+  co2_power_on();
+  // 10 second warmup
+  delay(10000);
+  int co2 = co2_get_measurement();
+  Serial.println("CO2: " + String(co2));  
+  co2_power_off();
+  
+  delay(5000);
 }
